708510c5e46d5e38b8ce0c932b8834facbed0825
Akash-Chhangani <akashchhangani4@gmail.com>
2024-02-15 15:16:58 +0530

    Before using router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-
 function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
@@ -24,10 +23,12 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark Mode has been enabled", "success");
+      document.title = "TextUtils-Dark Mode";
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light Mode has been enabled", "success");
+      document.title = "TextUtils-Light Mode";
     }
   };
   return (
